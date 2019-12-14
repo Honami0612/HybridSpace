83b9e5f888d3703b2a4d29c714dc085b2325bfc0
Honami0612 <ciho16007@g.nihon-u.ac.jp>
2019-12-14 18:58:30 +0100

    basetutorial

---
+++ Assets/Script/Door.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    //[SerializeField]
+    //Text check;
+
+    public int doorNumber;
+    ModeChange modeChange;
+    private int modeNumber;
+
+    FadeController fadeController;
+
+	// Use this for initialization
+	void Start () {
+        modeChange = GameObject.FindWithTag("Player").GetComponent<ModeChange>();
+        fadeController = GameObject.Find("Panel").GetComponent<FadeController>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        modeNumber = modeChange.nowNumber;
+        Debug.Log(modeNumber);
+        //check.text = modeNumber.ToString()+""+doorNumber.ToString();
+    }
+
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (doorNumber == modeNumber)
+            {
+                //check.text = "true";
+                Debug.Log("doorClear");
+                fadeController.isFadeOut = true;
+                StartCoroutine(ChangeScene());
+            }
+        }
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (SceneManager.GetActiveScene().name == "Tutorial1")
+        {
+            SceneManager.LoadScene("Tutorial2");
+        }else if (SceneManager.GetActiveScene().name == "Tutorial2")
+        {
+            //GameStart
+        }
+        
+    }
+}
--- Assets/Script/ModeChange.cs
+++ Assets/Script/ModeChange.cs
@@ -1,24 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModeChange : MonoBehaviour {
+
+    [SerializeField]
+    Text check;
 
     SpriteRenderer spriteRenderer;
 
     [SerializeField]
     Sprite[] changeMode;
-    int modeNumber = 0;
+    public int modeNumber = 1;
+
+    [SerializeField]
+    Text lifeText;
+    public int playerLife;
+
+    string modeState;
+    
 
 	// Use this for initialization
 	void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        lifeText.text = "Life:" + playerLife.ToString();
+        check.text = modeNumber.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         Mode();
+        ChangeModeState();
+
 	}
 
     void Mode()
@@ -38,5 +54,42 @@ public class ModeChange : MonoBehaviour {
             }
           
         }
+        check.text = modeNumber.ToString();
     }
+
+    void ChangeModeState()
+    {
+        switch (modeNumber)
+        {
+            case 1:
+                modeState = "Normal";
+                break;
+            case 2:
+                modeState = "Fire";
+                break;
+            case 3:
+                modeState = "Thunder";
+                break;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            playerLife -= 20;
+            lifeText.text = "Life:"+playerLife.ToString();
+        }
+    }
+
+    public int nowNumber
+    {
+        get { return modeNumber; }
+        set { modeNumber = value; }
+    }
+
+    
+
+
 }
