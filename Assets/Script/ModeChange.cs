5c7aa34f728b4da0869ea776a6f98daab5301dd9
Honami0612 <ciho16007@g.nihon-u.ac.jp>
2019-12-14 13:19:20 +0100

    add background

---
+++ Assets/Script/ModeChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeChange : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    Sprite[] changeMode;
+    int modeNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Mode();
+	}
+
+    void Mode()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (modeNumber < changeMode.Length)
+            {
+                spriteRenderer.sprite = changeMode[modeNumber];
+                modeNumber++;
+            }
+            else
+            {
+                modeNumber = 0;
+                spriteRenderer.sprite = changeMode[modeNumber];
+                modeNumber++;
+            }
+          
+        }
+    }
+}
