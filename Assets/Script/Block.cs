6cb36418cf39cc7d9bc0f84b07d4c0136970018d
Honami0612 <ciho16007@g.nihon-u.ac.jp>
2019-12-15 13:50:58 +0100

    a

---
+++ Assets/Script/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public bool breakDown=false;
+    public bool nobreak=false;
+
+   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (breakDown)
+            {
+                Destroy(this.gameObject);
+            }else if (nobreak)
+            {
+                //this.gameObject.GetComponent<>
+            }
+        } 
+    }
+}
