51c94ed1714fdcca8f4d630f8a425892b5de3595
Honami0612 <ciho16007@g.nihon-u.ac.jp>
2019-12-18 14:56:15 +0100

    fix

---
+++ Assets/Particles/StartMenu/NextLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Tutorial1");
+    }
+}
