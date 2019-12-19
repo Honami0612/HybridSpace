faa4f9534f08fbd28ecd314f59cf41b3be2b9d77
noy4 <naoyan418@gmail.com>
2019-12-19 05:24:51 +0100

    commit leak

---
+++ Assets/Script/Others/Restarter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restarter : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Dead");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Dead");
+            SceneManager.LoadScene("Stage 1");
+        }
+    }
+}
